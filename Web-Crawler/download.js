4f82aa828cd25a8203e896828986e109535ecc55
jimyandres <jimyandres@utp.edu.co>
2018-01-24 17:50:58 -0500

    Gitignore updated and download.js added

---
+++ Web-Crawler/download.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const uuidv1 = require('uuid/v1');
+
+const downloadPage = (url = 'for generating random timestamped values') => {
+  const fetchPage = (urlF, callback) => {
+    http.get(urlF, (response) => {
+      let buff = '';
+      response.on('data', (chunk) => {
+        buff += chunk;
+      });
+      response.on('end', () => {
+        callback(null, buff);
+      });
+    }).on('error', (error) => {
+      console.log(`Got error: ${error.message}`);
+      callback(error);
+    });
+  };
+
+  const folderName = 'f_' + uuidv1();
+  fs.mkdirSync(folderName);
+
+  fetchPage(url, (error, data) => {
+    if (error) return console.log(err);;
+    fs.writeFileSync(path.join(__dirname, folderName, 'url.txt'), url);
+    fs.writeFileSync(path.join(__dirname, folderName, 'file.html'), data);
+    console.log(`Downloading is done in folder ${folderName}`);
+  });
+}
+
+downloadPage(process.argv[2]);
