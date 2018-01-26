00c544b93af5ad5473114beca4a835829bc76b4a
jimyandres <jimyandres@utp.edu.co>
2018-01-26 17:04:59 -0500

    Minor changes: delete unnecessary data

--- Module_02/Blog-server/routes/comments.js
+++ Module_02/Blog-server/routes/comments.js
@@ -1,21 +1,21 @@
 // GET all comments of a post
-const get = (store) => (req, res, store) => {
+const get = (store) => (req, res) => {
   const postId  = req.params.postId;
   res.status(200).send(store.posts[postId].comments);
 };
 
 // POST a new comment
-const post = (store) => (req, res, store) => {
+const post = (store) => (req, res) => {
   const postId  = req.params.postId;
   const newComment = req.body;
-  const commentId = store.posts.lenght;
+  const commentId = store.posts[postId].comments.lenght;
   store.posts[postId].comments.push(newComment);
   console.log('New comment: \t', store.posts[postId].comments);
   res.status(201).send({commentId:commentId});
 };
 
 // PUT an existing post
-const put = (store) => (req, res, store) => {
+const put = (store) => (req, res) => {
   const postId  = req.params.postId;
   const commentId  = req.params.commentId;
   store.posts[postId].comments[commentId] = req.body;
@@ -24,10 +24,10 @@ const put = (store) => (req, res, store) => {
 };
 
 // DELETE an existing post
-const deleteComment = (store) => (req, res, store) => {
+const deleteComment = (store) => (req, res) => {
   const postId  = req.params.postId;
   const commentId  = req.params.commentId;
-  store.posts[postId].comments[commentId].splice(commentId, 1);
+  store.posts[postId].comments.splice(commentId, 1);
   console.log('New store: ', store.posts);
   res.status(204).send();
 };
