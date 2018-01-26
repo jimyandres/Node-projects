a772118684ed29e5736007ace7065c25da33b755
jimyandres <jimyandres@utp.edu.co>
2018-01-26 13:59:18 -0500

    Minor changes to get a better info in the server log

--- Module_02/rest-api/server.js
+++ Module_02/rest-api/server.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(errorhandler());
 
+app.use((req, res, next) => {
+  console.log('Current store: \t', store.accounts);
+  next();
+});
+
 app.get('/accounts', (req, res) => {
   res.status(200).send(store.accounts);
 });
@@ -19,16 +24,19 @@ app.post('/accounts', (req, res) => {
   let newAccount = req.body;
   let id = store.accounts.lenght;
   store.accounts.push(newAccount);
+  console.log('New store: ', store.accounts);
   res.status(201).send({id:id});
 });
 
 app.put('/accounts/:id', (req, res) => {
   store.accounts[req.params.id] = req.body;
+  console.log('New store: \t', store.accounts);
   res.status(200).send(store.accounts[req.params.id]);
 });
 
 app.delete('/accounts/:id', (req, res) => {
   store.accounts.splice(req.params.id, 1);
+  console.log('New store: ', store.accounts);
   res.status(204).send();
 });
 
