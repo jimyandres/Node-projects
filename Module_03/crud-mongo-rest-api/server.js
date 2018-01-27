b2e8b23060528b9783f4da31178ee517c80984af
jimyandres <jimyandres@utp.edu.co>
2018-01-27 10:48:23 -0500

    Server updated

---
+++ Module_03/crud-mongo-rest-api/server.js
@@ -0,0 +1,33 @@
+// set up ======================================================================
+const express = require('express');
+const logger = require('morgan');
+const errorhandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const database = require('./config/database'); // load the database configuration
+const app = express();
+
+// configuration ===============================================================
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+// routes ======================================================================
+const routes = require('./routes');
+const url = database.remoteUrl? database.remoteUrl : database.localUrl;
+mongodb.MongoClient.connect(url, (error, client) => {
+  if (error) return process.exit(1);
+  console.log("Connected successfully to server");
+
+  const db = client.db(database.name);
+
+  app.get('/accounts', routes.accounts.get(db));
+  app.post('/accounts', routes.accounts.post(db));
+  app.put('/accounts/:id', routes.accounts.put(db));
+  app.delete('/accounts/:id', routes.accounts.del(db));
+
+
+  // listen (star app with node server.js) =====================================
+  app.listen(3000);
+  console.log('App listening on port 3000');
+
+});
